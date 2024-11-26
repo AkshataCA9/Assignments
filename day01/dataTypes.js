1f245de80668ca96ad22b3fa5d28830403173fb5
AkshataCA9 <akshataca9@gmail.com>
2024-11-26 22:19:39 +0530

    Add files via upload

---
+++ day01/dataTypes.js
@@ -0,0 +1,16 @@
+let firstName = "Akshata";
+let companyName = 'TechSolutions';
+let mobileNumber = +918618527197;
+let isAutomation = true;
+let hasPlaywright;
+let interest = 12.30%
+console.log("The datatype of firstName is " + typeof firstName);
+console.log("The datatype of companyname is "+typeof companyName);
+console.log("The datatype of mobileNumber is "+typeof mobileNumber);
+console.log("The datatype of isAutomation is "+typeof isAutomation);
+console.log("The datatype of hasPlaywright is "+typeof hasPalywright);
+console.log("The datatype of interest is "+typeof interest);
+
+
+
+
